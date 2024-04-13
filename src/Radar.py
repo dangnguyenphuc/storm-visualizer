651531ee74a3fe304cff211ab542279b1c6ecbda
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2024-04-14 00:31:07 +0700

    1.0.0 - #3c. Hotfix: Choose data folder error - First modified Radar.py file

--- src/Radar.py
+++ src/Radar.py
@@ -9,64 +9,54 @@ import xarray
 import pandas as pd
 from tint.grid_utils import *
 from Config import *
-
-class DIRECTORY:
-  FILE_PATH = DEFAULT_FILE_PATH
-  RADAR_NAME = DEFAULT_RADAR_NAME
-  YEAR = DEFAULT_YEAR
-  MONTH = DEFAULT_MONTH
-  DATE = DEFAULT_DATE
-  MODE = DEFAULT_MODE
-
-  @staticmethod
-  def getCurrentPath(filename = False, radar = False, year = False, month = False, date = False, mode = False):
-    if filename:
-      return DIRECTORY.FILE_PATH
-
-    if radar:
-      return DIRECTORY.FILE_PATH + DIRECTORY.RADAR_NAME
-
-    if year:
-      return DIRECTORY.FILE_PATH + DIRECTORY.RADAR_NAME + DIRECTORY.YEAR
-
-    if month:
-      return DIRECTORY.FILE_PATH + DIRECTORY.RADAR_NAME + DIRECTORY.YEAR + DIRECTORY.MONTH
-
-    if date:
-      return DIRECTORY.FILE_PATH + DIRECTORY.RADAR_NAME + DIRECTORY.YEAR + DIRECTORY.MONTH + DIRECTORY.DATE
-
-    if mode:
-      return DIRECTORY.FILE_PATH + DIRECTORY.RADAR_NAME + DIRECTORY.YEAR + DIRECTORY.MONTH + DIRECTORY.DATE + DIRECTORY.MODE
-
 class DataManager:
 
-  RAW_DATA = None
-  GRID_DATA = None
-  NUM_OF_RAW_FILES = 0
-  NUM_OF_GRID_FILES = 0
+  def __init__(self, filePath: str = DEFAULT_FILE_PATH, radarName: str = DEFAULT_RADAR_NAME, year: str = DIRECTORY.YEAR, month: str = DIRECTORY.MONTH, day: str = DIRECTORY.DATE, mode: str = DIRECTORY.MODE):
+    if filePath is not None and filePath[-1] != "/": filePath += "/"
+    if radarName is not None and radarName[-1] != "/": radarName += "/"
+    if year is not None and year[-1] != "/": year += "/"
+    if month is not None and month[-1] != "/": month += "/"
+    if day is not None and date[-1] != "/": date += "/"
+    if filePath is not None and mode[-1] != "/": mode += "/"
+    
+    self.filePath = filePath
+    self.radarName = radarName
 
-  @staticmethod
-  def getAllDataFilePaths(filePath: str = DIRECTORY.FILE_PATH, radarName: str = DIRECTORY.RADAR_NAME, date: str =DIRECTORY.YEAR + DIRECTORY.MONTH + DIRECTORY.DATE, mode: str = DIRECTORY.MODE):
-    filePaths = [
-        filePath + radarName + date + mode + fileName for fileName in DataManager.listAllFile(
-        filePath,
-        radarName,
-        date,
-        mode)
-        ]
+    self.year = year
+    self.month = month
+    self.day = day
 
-    if "grid" not in mode:
-      DataManager.NUM_OF_RAW_FILES = len(filePaths)
+    if self.year is None or self.month is None or self.day is None:
+      self.date = None
     else:
-      DataManager.NUM_OF_GRID_FILES = len(filePaths)
+      self.date = self.year + self.month + self.day
+    self.mode = mode
 
+    if self.filePath is None or self.radarName is None or self.date is None or self.mode is None:
+      self.raw_data = None
+    else: self.raw_data = self.getAllDataFilePaths()
+    
+  def clearAll(self):
+    self.filePath = None
+    self.radarName = None
+    self.year = None
+    self.month = None
+    self.day = None
+    self.date = None
+    self.mode = None
+    self.raw_data = None
+
+  def getAllDataFilePaths(self):
+    filePaths = [
+        self.filePath + self.radarName + self.date + self.mode + fileName for fileName in self.listAllFile(
+        self.filePath,
+        self.radarName,
+        self.date,
+        self.mode)
+        ]
     return filePaths
 
-  @staticmethod
-  def reconstructFile(filePath: str = DIRECTORY.FILE_PATH):
-
-    if os.path.samefile(filePath, DEFAULT_FILE_PATH):
-      return
+  def reconstructFile(self):
 
     def loopThroughFiles(files):
       loopPath = []
@@ -92,37 +82,35 @@ class DataManager:
         except:
           continue
 
-
       for path in loopPath:
-        DataManager.splitData(filePath=path["filePath"], radarName=path["radarName"], date=path["date"], mode="")
+        self.splitData(filePath=path["filePath"], radarName=path["radarName"], date=path["date"], mode="")
 
     try:
-      if filePath[-1] != '/': filePath += '/'
-      folder = listDirInDir(filePath)
+      if self.filePath[-1] != '/': self.filePath += '/'
+      folder = listDirInDir(self.filePath)
       if len(folder) > 0:
         currentFolder = folder[0]
         while len(folder):
-          for item in os.listdir(filePath + currentFolder):
-            os.rename(filePath + currentFolder + "/" + item, filePath + item)
+          for item in os.listdir(self.filePath + currentFolder):
+            os.rename(self.filePath + currentFolder + "/" + item, self.filePath + item)
 
-          os.rmdir(filePath + currentFolder)
-          folder = listDirInDir(filePath)
+          os.rmdir(self.filePath + currentFolder)
+          folder = listDirInDir(self.filePath)
           if len(folder) > 0:
             currentFolder = folder[0]
           else: 
-            print(f'{filePath} is empty.')
+            print(f'{self.filePath} is empty.')
             break
 
-      files = listFile(filePath)
+      files = listFile(self.filePath)
       if len(files):
         loopThroughFiles(files=files)
     except Exception as e:
       print(e)
 
-  @staticmethod
-  def splitData(filePath: str = DIRECTORY.FILE_PATH, radarName: str = DIRECTORY.RADAR_NAME, date: str =DIRECTORY.YEAR + DIRECTORY.MONTH + DIRECTORY.DATE, mode: str = DIRECTORY.MODE):
-    if mode == "raw/" or not mode or mode == "":
-      data = DataManager.getAllDataFilePaths(filePath, radarName, date, mode)
+  def splitData(self):
+    if self.mode == "raw/" or not self.mode or self.mode == "":
+      data = self.getAllDataFilePaths()
       fixed = []
       dual = []
       staggered = []
@@ -137,19 +125,19 @@ class DataManager:
         else: other.append(data[i])
 
       if len(fixed) > 0:
-        firstDir = filePath + radarName + date + 'fixed/'
+        firstDir = self.filePath + self.radarName + self.date + 'fixed/'
         if not os.path.exists(firstDir):
           os.mkdir(firstDir)
       if len(dual) > 0:
-        secondDir = filePath + radarName + date + 'dual/'
+        secondDir = self.filePath + self.radarName + self.date + 'dual/'
         if not os.path.exists(secondDir):
           os.mkdir(secondDir)
       if len(staggered) > 0:
-        thirdDir = filePath + radarName + date + 'staggered/'
+        thirdDir = self.filePath + self.radarName + self.date + 'staggered/'
         if not os.path.exists(thirdDir):
           os.mkdir(thirdDir)
       if len(other) > 0:
-        fourthDir = filePath + radarName + date + 'staggered/'
+        fourthDir = self.filePath + self.radarName + self.date + 'staggered/'
         if not os.path.exists(fourthDir):
           os.mkdir(fourthDir)
 
@@ -162,25 +150,24 @@ class DataManager:
       for i in other:
         os.rename(i, fourthDir + i.split('/')[-1])
 
-      if mode: os.rmdir(filePath + radarName + date + mode)
+      if self.mode: os.rmdir(self.filePath + self.radarName + self.date + self.mode)
 
-  @staticmethod
-  def genGrid(filePath: str = DIRECTORY.FILE_PATH, radarName: str = DIRECTORY.RADAR_NAME, date: str =DIRECTORY.YEAR + DIRECTORY.MONTH + DIRECTORY.DATE, mode: str = DIRECTORY.MODE):
+  def genNhaBeRadarGrid(self):
     def get_grid(radar):
       """ Returns grid object from radar object. """
       grid = pyart.map.grid_from_radars(
-          radar, grid_shape=(5, 1000, 1000),
-          grid_limits=((0, 25000), (-330000,330000), (-330000, 330000)),
+          radar, grid_shape=(50, 1000, 1000),
+          grid_limits=((0, 25000), (-300000,300000), (-300000, 300000)),
           fields=['reflectivity'], gridding_algo='map_gates_to_grid',
           h_factor=0., nb=0.6, bsp=1., min_radius=200.)
       return grid
 
     def write_grid():
-        tmp_dir = filePath + radarName + date + "grid/" + mode
+        tmp_dir = self.filePath + self.radarName + self.date + "grid/" + self.mode
         if not os.path.exists(tmp_dir):
           os.makedirs(tmp_dir)
 
-        keys = DataManager.getAllDataFilePaths(filePath=filePath, radarName=radarName, date=date, mode=mode)
+        keys = self.getAllDataFilePaths()
         for key in keys:
             radar = pyart.io.read(key)
             grid = get_grid(radar)
@@ -189,69 +176,80 @@ class DataManager:
 
     write_grid()
 
-  @staticmethod
-  def listAllRadar(filePath=DIRECTORY.FILE_PATH):
-    return listDirInDir(filePath)
+  def listAllRadar(self):
+    return listDirInDir(self.filePath)
 
-  @staticmethod
-  def listAllDateOfRadar(filePath=DIRECTORY.FILE_PATH, radar=DIRECTORY.RADAR_NAME):
-    years = [year for year in listDirInDir(filePath+radar) if year.isdigit()]
+  def listAllDateOfRadar(self):
+    years = [year for year in listDirInDir(self.filePath + self.radarName) if year.isdigit()]
     if len(years) == 0:
       return None
 
     months = []
     for year in years:
-      months += [year + "/" + month for month in listDirInDir(filePath + radar + year) if month.isdigit() and int(month) <= 12 and int(month) > 0]
+      months += [year + "/" + month for month in listDirInDir(self.filePath + self.radarName + year) if month.isdigit() and int(month) <= 12 and int(month) > 0]
     if len(months) == 0:
       return None
 
     dates = []
     for month in months:
-      dates += [month + "/" + date for date in listDirInDir(filePath + radar + month) if date.isdigit() and is_valid_day_for_month_year(date, month)]
+      dates += [month + "/" + date for date in listDirInDir(self.filePath + self.radarName + month) if date.isdigit() and is_valid_day_for_month_year(date, month)]
     if len(dates) == 0:
       return None
 
     return dates
 
-  @staticmethod
-  def listAllModeOnDate(filePath=DIRECTORY.FILE_PATH, radar=DIRECTORY.RADAR_NAME, date=DIRECTORY.YEAR+DIRECTORY.MONTH+DIRECTORY.DATE):
-    mode = listDirInDir(filePath+radar+date)
+  def listAllModeOnDate(self):
+    mode = listDirInDir(self.filePath + self.radarName + self.date)
+    # remove grid folder
     if "grid" in mode:
       mode.remove("grid")
     return mode
 
-  @staticmethod
-  def listAllFile(filePath=DIRECTORY.FILE_PATH, radar=DIRECTORY.RADAR_NAME, date=DIRECTORY.YEAR+DIRECTORY.MONTH+DIRECTORY.DATE, mode=DIRECTORY.MODE):
-    return listFile(filePath + radar + date + mode)
+  def listAllFile(self):
+    return listFile(self.filePath + self.radarName + self.date + self.mode)
+
+  def getCurrentPath(self, filename = False, radar = False, year = False, month = False, date = False, mode = False):
+    if filename:
+      return self.filePath
+
+    if radar:
+      return self.filePath + self.radarName
+
+    if year:
+      return self.filePath + self.radarName + self.year
+
+    if month:
+      return self.filePath + self.radarName + self.year + self.month
+
+    if date:
+      return self.filePath + self.radarName + self.date
+
+    if mode:
+      return self.filePath + self.radarName + self.date + self.mode
 
 class Radar:
 
-  def __init__(self, fileIndex = 0, filePath: str = DIRECTORY.FILE_PATH, radarName: str = DIRECTORY.RADAR_NAME, date: str =DIRECTORY.YEAR + DIRECTORY.MONTH + DIRECTORY.DATE, mode: str = DIRECTORY.MODE):
-    DataManager.RAW_DATA = DataManager.getAllDataFilePaths(filePath, radarName, date, mode)
+  def __init__(self, fileIndex: int = 0, filePath = DEFAULT_FILE_PATH, radarName = DEFAULT_RADAR_NAME, year = DEFAULT_YEAR, month = DEFAULT_MONTH, day = DEFAULT_DATE, mode = DEFAULT_MODE):
+    self.setDataManger()
 
     self.currentIndex = fileIndex
     self.getRadar()
 
+  def setDataManger(self, filePath = DEFAULT_FILE_PATH, radarName = DEFAULT_RADAR_NAME, year = DEFAULT_YEAR, month = DEFAULT_MONTH, day = DEFAULT_DATE, mode = DEFAULT_MODE):
+    self.DataManager = DataManager(filePath=filePath, radarName=radarName, year=year, month=month, day=day, mode=mode)
+
   def getRadar(self):
-    self.data = pyart.io.read(DataManager.RAW_DATA[self.currentIndex])
+    self.data = pyart.io.read(self.DataManager.raw_data[self.currentIndex])
     self.currentReflectivity = self.data.fields['reflectivity']['data'].flatten()
 
   def increaseIndex(self):
     self.currentIndex += 1
 
-    if (self.currentIndex >= DataManager.NUM_OF_RAW_FILES):
+    if (self.currentIndex >= len(self.DataManager.raw_data)):
       self.currentIndex = 0
 
   def readDataFromFilePath(self, filePath: str):
     self.data = pyart.io.read(filePath)
-
-  def readDataFromOtherMode(self, mode: str, fileIndex: int = 0):
-    data = DataManager.getAllDataFilePaths(mode = mode)
-    self.data = pyart.io.read(data[fileIndex])
-
-  def readDataFromOtherDate(self, date: str, mode: str, fileIndex: int = 0):
-    data = DataManager.getAllDataFilePaths(date = date, mode=mode)
-    self.data = pyart.io.read(data[fileIndex])
 
   def update(self, index = None):
     if index is not None:
@@ -330,72 +328,73 @@ class Radar:
 
     else:
       self.currentReflectivity = self.data.fields['reflectivity']['data'].flatten()
-class Grid:
 
-  def __init__(self, fileIndex = 0, filePath: str = DIRECTORY.FILE_PATH, radarName: str = DIRECTORY.RADAR_NAME, date: str =DIRECTORY.YEAR + DIRECTORY.MONTH + DIRECTORY.DATE, mode: str = DIRECTORY.MODE):
-    if not DataManager.GRID_DATA:
-      DataManager.GRID_DATA = DataManager.getAllDataFilePaths(filePath, radarName, date, "grid/" + mode)
+# class Grid:
 
-    self.currentIndex = fileIndex
-    self.getGrid()
+#   def __init__(self, fileIndex = 0, filePath: str = DIRECTORY.FILE_PATH, radarName: str = DIRECTORY.RADAR_NAME, date: str =DIRECTORY.YEAR + DIRECTORY.MONTH + DIRECTORY.DATE, mode: str = DIRECTORY.MODE):
+#     if not DataManager.GRID_DATA:
+#       DataManager.GRID_DATA = DataManager.getAllDataFilePaths(filePath, radarName, date, "grid/" + mode)
 
-  def getGrid(self):
-    self.data = pyart.io.read_grid(DataManager.GRID_DATA[self.currentIndex])
-    self.currentReflectivity = self.data.fields['reflectivity']['data']          
+#     self.currentIndex = fileIndex
+#     self.getGrid()
 
-  def increaseIndex(self):
-    self.currentIndex += 1
+#   def getGrid(self):
+#     self.data = pyart.io.read_grid(DataManager.GRID_DATA[self.currentIndex])
+#     self.currentReflectivity = self.data.fields['reflectivity']['data']          
 
-    if (self.currentIndex >= DataManager.NUM_OF_GRID_FILES):
-      self.currentIndex = 0
+#   def increaseIndex(self):
+#     self.currentIndex += 1
 
-  def readDataFromFilePath(self, filePath: str):
-    self.data = pyart.io.read_grid(filePath)
+#     if (self.currentIndex >= DataManager.NUM_OF_GRID_FILES):
+#       self.currentIndex = 0
 
-  def readDataFromOtherMode(self, mode: str, fileIndex: int = 0):
-    data = DataManager.getAllDataFilePaths(mode = "grid/" + mode)
-    self.data = pyart.io.read_grid(data[fileIndex])
+#   def readDataFromFilePath(self, filePath: str):
+#     self.data = pyart.io.read_grid(filePath)
 
-  def readDataFromOtherDate(self, date: str, mode: str, fileIndex: int = 0):
-    data = DataManager.getAllDataFilePaths(date = date, mode="grid/" + mode)
-    self.data = pyart.io.read_grid(data[fileIndex])
+#   def readDataFromOtherMode(self, mode: str, fileIndex: int = 0):
+#     data = DataManager.getAllDataFilePaths(mode = "grid/" + mode)
+#     self.data = pyart.io.read_grid(data[fileIndex])
 
-  def update(self, index = None):
-    if index is not None:
-      self.currentIndex = index
-    else:
-      self.increaseIndex()
+#   def readDataFromOtherDate(self, date: str, mode: str, fileIndex: int = 0):
+#     data = DataManager.getAllDataFilePaths(date = date, mode="grid/" + mode)
+#     self.data = pyart.io.read_grid(data[fileIndex])
 
-    self.getGrid()
+#   def update(self, index = None):
+#     if index is not None:
+#       self.currentIndex = index
+#     else:
+#       self.increaseIndex()
 
-  def get_vertices_position(self, scaler):
-    a = self.data.z['data']
-    b = self.data.y['data']
-    c = self.data.x['data']
+#     self.getGrid()
 
-    # Create all combinations of indices
-    idx_a, idx_b, idx_c = np.meshgrid(np.arange(len(a)), np.arange(len(b)), np.arange(len(c)), indexing='ij')
-    combinations = np.array([c[idx_c.ravel()], b[idx_b.ravel()], a[idx_a.ravel()]]).T
-    return scaler.fit_transform(
-        combinations
-    )
+#   def get_vertices_position(self, scaler):
+#     a = self.data.z['data']
+#     b = self.data.y['data']
+#     c = self.data.x['data']
 
-  def get_all_vertices_by_threshold(self, threshold = 0):
+#     # Create all combinations of indices
+#     idx_a, idx_b, idx_c = np.meshgrid(np.arange(len(a)), np.arange(len(b)), np.arange(len(c)), indexing='ij')
+#     combinations = np.array([c[idx_c.ravel()], b[idx_b.ravel()], a[idx_a.ravel()]]).T
+#     return scaler.fit_transform(
+#         combinations
+#     )
 
-    # indices = np.where(np.logical_and(np.logical_not(self.currentReflectivity.mask),self.currentReflectivity.data >= threshold))
+#   def get_all_vertices_by_threshold(self, threshold = 0):
 
-    # storm Identification testing
-    # grid_size = get_grid_size(self.data)
-    # min_size = 4 / np.prod(grid_size[1:]/1000)
-    # masked = self.data.fields['reflectivity']['data']
-    # masked.data[masked.data == masked.fill_value] = 0
-    # frame = get_filtered_frame(masked.data, min_size,
-    #                            32)
-    # indices = np.where(self.currentReflectivity > threshold)
+#     # indices = np.where(np.logical_and(np.logical_not(self.currentReflectivity.mask),self.currentReflectivity.data >= threshold))
 
-    scaler = MinMaxScaler(feature_range=(-1.0, 1.0))
-    vertices = self.get_vertices_position(scaler)
-    return {
-        'position': vertices[indices],
-        'color': color(self.currentReflectivity[indices])
-    }
+#     # storm Identification testing
+#     # grid_size = get_grid_size(self.data)
+#     # min_size = 4 / np.prod(grid_size[1:]/1000)
+#     # masked = self.data.fields['reflectivity']['data']
+#     # masked.data[masked.data == masked.fill_value] = 0
+#     # frame = get_filtered_frame(masked.data, min_size,
+#     #                            32)
+#     # indices = np.where(self.currentReflectivity > threshold)
+
+#     scaler = MinMaxScaler(feature_range=(-1.0, 1.0))
+#     vertices = self.get_vertices_position(scaler)
+#     return {
+#         'position': vertices[indices],
+#         'color': color(self.currentReflectivity[indices])
+#     }
